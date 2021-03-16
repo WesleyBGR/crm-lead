6100535b5fea9a09dc0585169b390e7992710bb5
WesleyBGR <51459980+WesleyBGR@users.noreply.github.com>
2021-03-16 15:43:55 -0300

    Add files via upload

---
+++ graph-api-webhooks-samples-master/heroku/index.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var xhub = require('express-x-hub');
+
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'));
+
+app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
+app.use(bodyParser.json());
+
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+});
+
+ 
+app.get('me/feed?access_token='+'EAAKLiWhz20EBAHLAmSisc62RNxdOTfOp1anvOReJWz9TmGoHKUXCz7QlJugMJOWZBwVZCHyOlGZCFZAYGTDq6ZAHqa1N3tQztZC4kOxTIgcsP0oVPf0QDSXJIaRI01msG31o8pZBn4LHQhJodDqfZBz9LfxCgPQroJ17iJZBBriZBBmhgZBHHLGvnMecfVKf1KWn5v3g2jsY0E4UwZDZD', function(req, res) {
+  console.log(req);
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
+});
+
+app.get(['/facebook', '/instagram'], function(req, res) {
+  if (
+    req.query['hub.mode'] == 'subscribe' &&
+    req.query['hub.verify_token'] == token
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/facebook', function(req, res) {
+  console.log('Facebook request body:', req.body);
+
+  if (!req.isXHubValid()) {
+    console.log('Warning - request header X-Hub-Signature not present or invalid');
+    res.sendStatus(401);
+    return;
+  }
+
+  console.log('request header X-Hub-Signature validated');
+  // Process the Facebook updates here
+  received_updates.unshift(req.body);
+  res.sendStatus(200);
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  received_updates.unshift(req.body);
+  res.sendStatus(200);
+});
+
+app.listen();
